b0fed7445ca674c7fe4f6bb2e653fe8961249440
Tomasz <tomasz.murglin@acoustic.co>
2021-04-12 10:13:31 +0200

    initial commit

---
+++ src/main/java/com/murglin/consulting/publishinghousereviewservice/PublishingHouseReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.murglin.consulting.publishinghousereviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublishingHouseReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublishingHouseReviewServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/murglin/consulting/publishinghousereviewservice/PublishingHouseReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murglin.consulting.publishinghousereviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublishingHouseReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
